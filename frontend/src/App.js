1340f2331217342feb81d1142a6a57145a05447c
ardato belay <aradatobelay@gmaill.com>
2020-10-18 13:00:28 +0300

    add product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 import {Container} from "react-bootstrap"
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
    
      <main className="py-3">
        <Container> 
-       <h1>Welcome To Proshop</h1>
+       <HomeScreen/>
        </Container>
      
      </main>
